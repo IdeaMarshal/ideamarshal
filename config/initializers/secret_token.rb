437a05ea840f5f5002dfd6c55dcf09c403e0f6ce
Taylor Yelverton <taylor@ideamarshal.com>
2012-02-26 17:44:54 -0600

    Initialized new rails project.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+IdeaMarshal::Application.config.secret_token = 'e6d5cc43d6c908cdc1fddf23fa6cd6939a728c0e2d35ebef3daad0066572206648cba98e63e10ed3bbad5f6b33f2a1e60af2faefd914794c76edefc90ae642fc'
